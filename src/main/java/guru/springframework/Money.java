76d0641cef7b4b7d4d4ae15ec917df40892e610e
Eric Williamson <ewilliamson@liberty.edu>
2019-07-04 13:30:50 -0400

    lesson 23

---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+public class Money {
+    protected int amount;
+
+    public static Dollar dollar(int amount){
+        return new Dollar(amount);
+    }
+
+    public boolean equals(Object object){
+        Money money = (Money) object;
+        return amount == money.amount && getClass().equals(object.getClass());
+    }
+}
